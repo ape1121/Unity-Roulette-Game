e001d15d2086f2f25daedbf2606d46339595a4f8
Alp <alpapaydin@windowslive.com>
2026-03-10 00:20:52 +0300

    organize folder structure, refactor roulette scriptable data

--- Assets/_Game/Scripts/Data/Roulette/RouletteWheelData.cs
+++ Assets/_Game/Scripts/Data/Roulette/RouletteWheelData.cs
@@ -1,22 +1,23 @@
 using UnityEngine;
-using UnityEngine.Scripting.APIUpdating;
 
 namespace Ape.Data
 {
-    [MovedFrom(false, sourceNamespace: "")]
     [CreateAssetMenu(fileName = "RouletteWheelData", menuName = "CriticalShot/Roulette/Roulette Wheel Data")]
     public sealed class RouletteWheelData : ScriptableObject
     {
+        [SerializeField] private RouletteZoneType zoneType;
         [SerializeField] private bool allowDuplicateRewards;
         [SerializeField] private RouletteSliceData[] sliceDefinitions;
         [Min(0.5f)] [SerializeField] private float spinDuration = 3.4f;
         [Min(1)] [SerializeField] private int fullRotations = 6;
         [Range(0f, 20f)] [SerializeField] private float settleOvershootDegrees = 6f;
+        [SerializeField] private Sprite wheelBackground;
 
         public bool AllowDuplicateRewards => allowDuplicateRewards;
         public RouletteSliceData[] SliceDefinitions => sliceDefinitions ?? System.Array.Empty<RouletteSliceData>();
         public float SpinDuration => Mathf.Max(0.5f, spinDuration);
         public int FullRotations => Mathf.Max(1, fullRotations);
         public float SettleOvershootDegrees => Mathf.Max(0f, settleOvershootDegrees);
+        public RouletteZoneType ZoneType => zoneType;
     }
 }
